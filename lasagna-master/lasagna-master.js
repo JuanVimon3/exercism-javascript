60ce26661a2cf31709c0194708d54c95db086781
JuanVimon3 <jvillamonsalve@gmail.com>
2023-04-01 14:32:57 -0500

    add lasagna master's exercise with eslint's suggestions

--- lasagna-master/lasagna-master.js
+++ lasagna-master/lasagna-master.js
@@ -1,7 +1,5 @@
 /// <reference path="./global.d.ts" />
 
-import { createUnzip } from "zlib"
-
 // @ts-check
 
 /**
@@ -14,45 +12,45 @@ import { createUnzip } from "zlib"
  * }
  */
 export function cookingStatus(time) {
-  if(time == 0){
+  if (time === 0) {
     return 'Lasagna is done.';
-  }else if(time > 0){
+  } if (time > 0) {
     return 'Not done, please wait.';
-  }else{
-    return 'You forgot to set the timer.';
   }
+  return 'You forgot to set the timer.';
 }
 
 export function preparationTime(layers,time = 2) {
- return layers.length * time
+  return layers.length * time;
 }
 
 export function quantities(layers) {
-let sauce = 0;
-let noodles = 0;
-let sauceCant = 0;
-let noodlesCant = 0;
-for(let i = 0; i < layers.length; i++) {
-  if(layers[i].includes('sauce')) {
-    sauceCant++
-  }else if(layers[i].includes('noodles')){
-    noodlesCant++
-  }
-  sauce = sauceCant * 0.2;
-  noodles = noodlesCant * 50
-}return {sauce, noodles}
+  let sauce = 0;
+  let noodles = 0;
+  let sauceCant = 0;
+  let noodlesCant = 0;
+  for (let i = 0; i < layers.length; i += 1) {
+    if (layers[i].includes('sauce')) {
+      sauceCant += 1;
+    } if (layers[i].includes('noodles')) {
+      noodlesCant += 1;
+    }
+    sauce = sauceCant * 0.2;
+    noodles = noodlesCant * 50;
+  } return { sauce, noodles };
 }
 
-export function addSecretIngredient(friendsList,myList) {
-let lastOne= friendsList[friendsList.length-1];
-myList.push(lastOne);
+export function addSecretIngredient(friendsList, myList) {
+  const lastOne = friendsList[friendsList.length - 1];
+  myList.push(lastOne);
 }
 
 export function scaleRecipe(recipe, amount) {
-  let cant= amount / 2;
-  let newRecipe = {};
-  for(let i in recipe){
-    newRecipe[i] = recipe[i] * cant
-  }
+  const cant = amount / 2;
+  const newRecipe = {};
+  Object.keys(recipe)
+    .forEach((i) => {
+      newRecipe[i] = recipe[i] * cant;
+    });
   return newRecipe;
 }
